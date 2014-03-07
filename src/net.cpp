f7711d4609813c5dc1f5b485ca79981d897cf0ea
ArekiboCoin <antonieg@arekibo.com>
2014-03-07 14:29:30 +0000

    changes

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"www.gaelcoin.org", "www.Gaelcoin.org"},
+    {"seed.gaelcoin.org", "seed.gaelcoin.org"},
     {"pool.Gaelcoin.org", "pool.Gaelcoin.org"},
     {NULL, NULL}
 };
